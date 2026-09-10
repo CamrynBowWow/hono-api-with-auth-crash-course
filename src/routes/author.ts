5ab6b0646f31b856e9db04a88570821c875e8695
CamrynBowWow <95950447+CamrynBowWow@users.noreply.github.com>
2026-09-10 13:59:05 +0200

    Author CRUD Implementation Done

--- src/routes/author.ts
+++ src/routes/author.ts
@@ -1,17 +1,31 @@
+import { sValidator } from '@hono/standard-validator';
 import { Hono } from 'hono';
+import z from 'zod';
 
 const app = new Hono();
 
-const authors = [
+const authors: { id: string; name: string; birthday?: Date | null }[] = [
 	{
 		id: '1',
 		name: 'Small',
+		birthday: new Date(),
 	},
 	{
 		id: '2',
 		name: 'Man',
 	},
 ];
+
+const createAuthorSchema = z.object({
+	name: z.string().min(1),
+	birthday: z.coerce.date().optional(),
+});
+
+//  nullable allows for removing of birthday
+const updateAuthorSchema = z.object({
+	name: z.string().min(1).optional(),
+	birthday: z.coerce.date().nullable().optional(),
+});
 
 app.get('/', (c) => {
 	return c.json(authors);
@@ -27,6 +41,51 @@ app.get('/:id', (c) => {
 	}
 
 	return c.json(author);
+});
+
+app.post('/', sValidator('json', createAuthorSchema), (c) => {
+	const data = c.req.valid('json');
+
+	const author = { id: crypto.randomUUID(), ...data };
+
+	authors.push(author);
+
+	return c.json(author, 201);
+});
+
+app.put('/:id', sValidator('json', updateAuthorSchema), (c) => {
+	const id = c.req.param('id');
+	const data = c.req.valid('json');
+
+	const author = authors.find((a) => a.id === id);
+
+	if (author == null) {
+		return c.json({ error: 'Author not found' }, 404);
+	}
+
+	if (data.name !== undefined) {
+		author.name = data.name;
+	}
+
+	if (data.birthday !== undefined) {
+		author.birthday = data.birthday;
+	}
+
+	return c.json(author);
+});
+
+app.delete('/:id', (c) => {
+	const id = c.req.param('id');
+
+	const index = authors.findIndex((a) => a.id === id);
+
+	if (index === -1) {
+		return c.json({ error: 'Author not found' }, 404);
+	}
+
+	authors.splice(index, 1);
+
+	return c.body(null, 204);
 });
 
 export default app;
